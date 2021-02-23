609c3d5573a10c3738755b2b2ef8b099882442ac
Neamen_Dobamo <Neamenbirhanu@gmail.com>
2021-02-22 19:37:41 -0700

    set _dirname and app listen

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const port = 8080;
+const mainDir = path.join(__dirname, "/public");
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+
+app.listen(port, function(){
+    console.log('take note port ${port}. ');
+
+})
